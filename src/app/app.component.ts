1e6adee75fd766a6e9b4695d13b8b5cfd1e229a4
David Bouhaben <davidbouhaben@yahoo.co.id>
2025-06-14 22:50:46 +0200

    update angular 20 and create a project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
+  imports: [RouterOutlet, UserComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Angular-19-Tutorial-FED-Learning';
