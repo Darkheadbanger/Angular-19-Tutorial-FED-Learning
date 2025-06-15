e8cc9385a21241c93e356456cecb50303138d05d
David Bouhaben <davidbouhaben@yahoo.co.id>
2025-06-15 14:01:55 +0200

    data-binging learn interpolation, property binding, event binding and two way ngModel binding

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { User } from './user/user';
+import { DataBinding } from './data-binding/data-binding';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, User],
+  imports: [RouterOutlet, User, DataBinding],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
